25078cfced336e6b6b24da71737d4609d6b94b80
Shashank Dev <coderperson043@gmail.com>
2026-09-11 17:50:04 +0530

    Expose detector interfaces

--- engine/search_rescue/__init__.py
+++ engine/search_rescue/__init__.py
@@ -4,6 +4,7 @@ from .models import (
     AerialDetection, DetectionBand, GeoPoint, SearchCell, SearchMission,
     SearchWaypoint, WaypointKind,
 )
+from .detectors import FrameMetadata, detect_change, detect_multispectral, detect_rgb, detect_thermal
 from .detection import fuse_detections
 from .prioritization import rank_search_cells
 from .coverage import plan_coverage
